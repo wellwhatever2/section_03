cbe504404046282d88dd6038e18e093f669af45a
Nonewthing <john@nonewthing.com>
2016-06-13 18:38:50 +1200

    first

---
+++ Source/Section_03/Section_03GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Section_03GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION_03_API ASection_03GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
